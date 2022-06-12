a4e1c29a818594d47380cd884d97889e208c66a2
ManeLippert <manuel-lippert@outlook.de>
2022-06-12 17:32:28 +0200

    improved shell script with autocomplete

--- ssh_copy.sh
+++ ssh_copy.sh
@@ -36,18 +36,12 @@ if [[ "$DESTADD" = "d" ]];then
             REMOTEINPUT=true
         # Make folder if necessary
         elif [[ "$REMOTEADD" = "m" ]] || [[ "$REMOTEADD" = "mj" ]] || [[ "$REMOTEADD" = "jm" ]]; then
-            if [[ "$COUNTREMOTE" = 1 ]]; then
-                echo "No need to make a directory"
+            if [[ "$DEST" = "remote" ]] || [[ "$DEST" = "r" ]]; then
+                ssh btrzx1-1.rz.uni-bayreuth.de "mkdir -p $REMOTEDIR"
+                REMOTEINPUT=true
             else
-                if [[ "$DEST" = "remote" ]] || [[ "$DEST" = "r" ]]; then
-                    ssh btrzx1-1.rz.uni-bayreuth.de "mkdir -p $REMOTEDIR"
-                    REMOTEINPUT=true
-                else
-                    echo "Directory has to exist on remote machine"
-                fi
+                echo "Directory has to exist on remote machine"
             fi
-        elif [[ "$REMOTEDIR" = "s" ]]; then
-            REMOTEINPUT=true
         # Context
         else
             if [[ "$DEST" = "remote" ]] || [[ "$DEST" = "r" ]]; then
@@ -57,48 +51,16 @@ if [[ "$DESTADD" = "d" ]];then
             fi
         fi
     done
-
-    # Check if remote directory do already exist in local machine
-    if [ -d "$REMOTEDIR" ]; then
-        LOCALDIR=$REMOTEDIR
-    else
-        # Check if local directory exists
-        while [[ $LOCALINPUT = false ]]; do
-            read -p "Local  dir: " -e LOCALDIR LOCALADD
-            if [ -d "$LOCALDIR" ]; then
-                LOCALINPUT=true    
-            # Make folder if necessary
-            elif [[ "$LOCALADD" = "m" ]]; then
-                if [[ "$DEST" = "local" ]] || [[ "$DEST" = "l" ]]; then
-                    mkdir -p $LOCALDIR
-                    LOCALINPUT=true
-                else
-                    echo "Directory has to exist on local machine"
-                fi
-            elif [[ "$LOCALDIR" = "s" ]]; then
-                LOCALINPUT=true
-            # Context
-            else
-                if [[ "$DEST" = "local" ]] || [[ "$DEST" = "l" ]]; then
-                    echo -e "Directory does not exist on local machine \n-> Create directory with 'dir m'"
-                else
-                    echo "Directory does not exist on local machine"
-                fi
-            fi
-        done
-    fi
-else
-    # Check if directory exists
-    while [[ $DIRINPUT = false ]]; do
-        read -p "Dir: " -e DIR DIRADD
-        # local
-        if [ -d "$DIR" ]; then
-            DIRINPUT=true
-        # Make folder if necessary on local machine
-        elif [[ "$DIRADD" = "m" ]] || [[ "$DIRADD" = "mj" ]] || [[ "$DIRADD" = "jm" ]]; then
+    # Check if local directory exists
+    while [[ $LOCALINPUT = false ]]; do
+        read -p "Local  dir: " -e LOCALDIR LOCALADD
+        if [ -d "$LOCALDIR" ]; then
+            LOCALINPUT=true    
+        # Make folder if necessary
+        elif [[ "$LOCALADD" = "m" ]]; then
             if [[ "$DEST" = "local" ]] || [[ "$DEST" = "l" ]]; then
                 mkdir -p $LOCALDIR
-                DIRINPUT=true
+                LOCALINPUT=true
             else
                 echo "Directory has to exist on local machine"
             fi
@@ -110,44 +72,64 @@ else
                 echo "Directory does not exist on local machine"
             fi
         fi
-        #remote
-        if [ ssh btrzx1-1.rz.uni-bayreuth.de "[ -d $REMOTEDIR ]" ]; then
-            DIRINPUT=true 
-        # Make folder if necessary on remote machine
+    done
+    # Copy files
+    if [[ "$DEST" = "remote" ]] ||  [[ "$DEST" = "r" ]]; then
+        scp -r $LOCALDIR/* bt712347@btrzx1-1.rz.uni-bayreuth.de:$REMOTEDIR
+        if [[ "$LOCALADD" = "j" ]] || [[ "$REMOTEADD" = "mj" ]] || [[ "$REMOTEADD" = "jm" ]]; then
+            ssh btrzx1-1.rz.uni-bayreuth.de "cp -r gkw/run/* $REMOTEDIR/"
+            ssh btrzx1-1.rz.uni-bayreuth.de "cd $REMOTEDIR && sbatch jobscript*"
+        fi
+    elif [[ "$DEST" = "local" ]] || [[ "$DEST" = "l" ]]; then
+        scp -r bt712347@btrzx1-1.rz.uni-bayreuth.de:$REMOTEDIR/* $LOCALDIR
+        rm -rf $LOCALDIR/gkw.*
+    fi
+#When directories are equal
+else
+    # Check if directory exists
+    while [[ $DIRINPUT = false ]]; do
+        read -p "Dir: " -e DIR DIRADD
+        # local
+        if [ -d "$DIR" ] && ssh btrzx1-1.rz.uni-bayreuth.de "[ -d $DIR ]"; then
+            DIRINPUT=true
+        # Make folder if necessary on local machine
         elif [[ "$DIRADD" = "m" ]] || [[ "$DIRADD" = "mj" ]] || [[ "$DIRADD" = "jm" ]]; then
-            if [[ "$DEST" = "remote" ]] || [[ "$DEST" = "r" ]]; then
-                ssh btrzx1-1.rz.uni-bayreuth.de "mkdir -p $REMOTEDIR"
+            # Local
+            if [[ "$DEST" = "local" ]] || [[ "$DEST" = "l" ]]; then
+                mkdir -p $DIR
+                DIRINPUT=true
+            # Remote
+            elif [[ "$DEST" = "remote" ]] || [[ "$DEST" = "r" ]]; then
+                ssh btrzx1-1.rz.uni-bayreuth.de "mkdir -p $DIR"
                 DIRINPUT=true
             else
-                echo "Directory has to exist on remote machine"
+                echo "Directory has to exist on machine"
             fi
         # Context
         else
+            # Local
             if [[ "$DEST" = "local" ]] || [[ "$DEST" = "l" ]]; then
+                echo -e "Directory does not exist on local machine \n-> Create directory with 'dir m'"
+            # Remote
+            elif [[ "$DEST" = "remote" ]] || [[ "$DEST" = "r" ]]; then
                 echo -e "Directory does not exist on remote machine \n-> Create directory with 'dir m'"
             else
-                echo "Directory does not exist on remote  machine"
+                echo "Directory does not exist"
             fi
         fi
     done
 
-    LOCALDIR=$DIR
-    LOCALADD=$DIRADD
-
-    REMOTEDIR=$DIR
-    REMOTEADD=$DIRADD
-fi
-
-# Copy files
-if [[ "$DEST" = "remote" ]] ||  [[ "$DEST" = "r" ]]; then
-    scp -r $LOCALDIR/* bt712347@btrzx1-1.rz.uni-bayreuth.de:$REMOTEDIR
-    if [[ "$LOCALADD" = "j" ]] || [[ "$REMOTEADD" = "mj" ]] || [[ "$REMOTEADD" = "jm" ]]; then
-        ssh btrzx1-1.rz.uni-bayreuth.de "cp -r gkw/run/* $REMOTEDIR/"
-        ssh btrzx1-1.rz.uni-bayreuth.de "cd $REMOTEDIR && sbatch jobscript*"
+    # Copy files
+    if [[ "$DEST" = "remote" ]] ||  [[ "$DEST" = "r" ]]; then
+        scp -r $DIR/* bt712347@btrzx1-1.rz.uni-bayreuth.de:$DIR
+        if [[ "$DIRADD" = "j" ]] || [[ "$DIRADD" = "mj" ]] || [[ "$DIRADD" = "jm" ]]; then
+            ssh btrzx1-1.rz.uni-bayreuth.de "cp -r gkw/run/* $DIR/"
+            ssh btrzx1-1.rz.uni-bayreuth.de "cd $DIR && sbatch jobscript*"
+        fi
+    elif [[ "$DEST" = "local" ]] || [[ "$DEST" = "l" ]]; then
+        scp -r bt712347@btrzx1-1.rz.uni-bayreuth.de:$DIR/* $DIR
+        rm -rf $DIR/gkw.*
     fi
-elif [[ "$DEST" = "local" ]] || [[ "$DEST" = "l" ]]; then
-    scp -r bt712347@btrzx1-1.rz.uni-bayreuth.de:$REMOTEDIR/* $LOCALDIR
-    rm -rf $LOCALDIR/gkw.*
 fi
 
 # Disconnect VPN
