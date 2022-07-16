a9b18a50acae0415f9b4f8e8276241daff38f9d5
ManeLippert <manuel-lippert@outlook.de>
2022-07-16 23:20:42 +0200

    scratch login option

--- ssh_btrzx1.sh
+++ ssh_btrzx1.sh
@@ -13,6 +13,9 @@ fi
 sleep 2
 ssh btrzx1-1.rz.uni-bayreuth.de
 
+# ssh in specfic folder
+#ssh -t btrzx1-1.rz.uni-bayreuth.de "cd /scratch/bt712347/ && bash --login"
+
 # Disconnect VPN
 if [[ "$CONNECTION" = "" ]]; then
     {
