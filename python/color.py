6bbc720f2b2cd4304ca82c49be04454a091f9c40
ManeLippert <manuel-lippert@outlook.de>
2023-02-26 23:32:40 +0100

    Colorpalette for plots

---
+++ python/color.py
@@ -0,0 +1,24 @@
+import seaborn as sns
+
+color = sns.color_palette("hls", 9).as_hex()
+
+print(color)
+
+pal_1 = ['#db5f57', '#dbb757', '#a7db57', '#57db5f', '#57dbb7', '#57a7db', '#5f57db', '#b757db', '#db57a7']
+
+pal_2 = ['#0173b2', '#de8f05', '#029e73', '#d55e00', '#cc78bc', '#ca9161', '#fbafe4', '#ece133', '#56b4e9']
+
+pal_3 = ['#d43d4f', '#f46d43', '#fdad60', '#fee08b', '#ffffbe', '#e6f598', '#aadca4', '#66c2a5', '#3387bc']
+
+
+'''
+1 1x1
+2 2x1
+3 2x2
+4 3x1
+5 3x1.5
+6 3x2.5
+7 3x3
+8 3x5
+9 4x1
+'''
