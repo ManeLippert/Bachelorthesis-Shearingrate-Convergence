e5fff2967523378cc78a58b2c291a7ce9294a880
ManeLippert <manuel-lippert@outlook.de>
2023-04-03 17:07:09 +0200

    Delete test python file

--- python/test.py
+++
@@ -1,9 +0,0 @@
-table_inner_width = 76
-
-        
-New version of slurm_monitor.py 
-
-Add: 
-- Integration of gkw_reset_checkpoint.py from Florian Rath
-- Script installs necessary modules by itself
-- 
